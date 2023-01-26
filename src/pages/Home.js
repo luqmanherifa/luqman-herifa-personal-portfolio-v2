bef621704c32ed626d4c09d89c77f70af4585366
luqmanherifa <99471792+luqmanherifa@users.noreply.github.com>
2023-01-26 22:23:33 +0700

    Update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export default function Home() {
   useEffect(function () {
-    document.title = "Luqman Herifa Portfolio";
+    document.title = "Luqman Herifa";
   }, []);
 
   return (
