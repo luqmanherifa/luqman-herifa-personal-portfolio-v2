a77862c42e345945d8aa4418f163b6cf70c90ed0
luqmanherifa <99471792+luqmanherifa@users.noreply.github.com>
2023-04-16 01:51:22 +0700

    Update

--- src/pages/components/WorksInterface.js
+++ src/pages/components/WorksInterface.js
@@ -13,10 +13,10 @@ export default function WorksInterfaceCp() {
 
   return (
     <div>
-      <div className="mx-auto my-10 max-w-5xl rounded-[2rem] border border-slate-700 py-12 dark:border-slate-400 lg:max-w-xl md:max-w-lg sm:my-6 sm:max-w-xs sm:rounded-3xl sm:py-6">
+      <div className="mx-auto my-7 max-w-5xl rounded-[2rem] border border-slate-700 py-12 dark:border-slate-400 lg:max-w-xl md:max-w-lg sm:my-6 sm:max-w-xs sm:rounded-3xl sm:py-6">
         <div className="text-center">
           <h3 className="mb-7 inline-block rounded-full bg-blue-400/10 px-5 py-2 text-xl font-bold text-blue-400 dark:bg-blue-400/20 sm:mb-5 sm:text-base">
-            User Interface Design
+            Interface Development
           </h3>
         </div>
         <div className="flex justify-center">
@@ -29,17 +29,26 @@ export default function WorksInterfaceCp() {
             <div className="flex max-w-5xl flex-wrap justify-center gap-7 sm:gap-5">
               {WorksInterface.map((workInterface) => {
                 return (
-                  <div
-                    key={workInterface.id}
-                    className="group relative max-w-md overflow-hidden rounded-2xl transition-all duration-500 hover:scale-95 sm:max-w-[16rem] sm:rounded-lg"
-                  >
-                    <a href={workInterface.link} target="_blank" rel="noreferrer">
-                      <img
-                        src={workInterface.image}
-                        alt=""
-                        className="cursor-pointer transition-all duration-500 group-hover:rotate-6 group-hover:scale-125"
-                      />
-                    </a>
+                  <div key={workInterface.id}>
+                    <div className="group relative max-w-md overflow-hidden rounded-2xl transition-all duration-500 hover:scale-95 sm:max-w-[16rem] sm:rounded-lg">
+                      <a href={workInterface.link} target="_blank" rel="noreferrer">
+                        <img
+                          src={workInterface.image}
+                          alt=""
+                          className="cursor-pointer transition-all duration-500 group-hover:rotate-6 group-hover:scale-125"
+                        />
+                      </a>
+                    </div>
+                    <div className="max-w-md sm:max-w-[16rem]">
+                      <div className="flex justify-between sm:block">
+                        <div className={workInterface.nd_css}>{workInterface.name}</div>
+                        <a href={workInterface.link} target="_blank" rel="noreferrer">
+                          <div className="mt-2 rounded-lg bg-zinc-400/10 py-2 px-3 text-sm text-zinc-200 hover:bg-zinc-100 hover:text-zinc-700 dark:text-zinc-600 dark:hover:bg-zinc-300">
+                            Figma
+                          </div>
+                        </a>
+                      </div>
+                    </div>
                   </div>
                 );
               })}
--- src/pages/components/WorksMobile.js
+++ src/pages/components/WorksMobile.js
@@ -13,7 +13,7 @@ export default function WorksMobileCp() {
 
   return (
     <div>
-      <div className="mx-auto my-10 max-w-5xl rounded-[2rem] border border-slate-700 py-12 dark:border-slate-400 lg:max-w-xl md:max-w-lg sm:my-6 sm:max-w-xs sm:rounded-3xl sm:py-6">
+      <div className="mx-auto my-7 max-w-5xl rounded-[2rem] border border-slate-700 py-12 dark:border-slate-400 lg:max-w-xl md:max-w-lg sm:my-6 sm:max-w-xs sm:rounded-3xl sm:py-6">
         <div className="text-center">
           <h3 className="mb-7 inline-block rounded-full bg-lime-400/10 px-5 py-2 text-xl font-bold text-lime-400 dark:bg-lime-400/20 sm:mb-5 sm:text-base">
             Mobile Development
@@ -29,17 +29,39 @@ export default function WorksMobileCp() {
             <div className="flex max-w-5xl flex-wrap justify-center gap-7 sm:gap-5">
               {WorksMobile.map((workMobile) => {
                 return (
-                  <div
-                    key={workMobile.id}
-                    className="group relative max-w-md overflow-hidden rounded-2xl transition-all duration-500 hover:scale-95 sm:max-w-[16rem] sm:rounded-lg"
-                  >
-                    <a href={workMobile.link} target="_blank" rel="noreferrer">
-                      <img
-                        src={workMobile.image}
-                        alt=""
-                        className="cursor-pointer transition-all duration-500 group-hover:rotate-6 group-hover:scale-125"
-                      />
-                    </a>
+                  <div key={workMobile.id}>
+                    <div className="group relative max-w-md overflow-hidden rounded-2xl transition-all duration-500 hover:scale-95 sm:max-w-[16rem] sm:rounded-lg">
+                      <a href={workMobile.link} target="_blank" rel="noreferrer">
+                        <img
+                          src={workMobile.image}
+                          alt=""
+                          className="cursor-pointer transition-all duration-500 group-hover:rotate-6 group-hover:scale-125"
+                        />
+                      </a>
+                    </div>
+                    <div className="max-w-md sm:max-w-[16rem]">
+                      <div className={workMobile.nd_css}>{workMobile.name}</div>
+                      <div className={workMobile.nd_css}>{workMobile.desc}</div>
+                      <div className="flex justify-between sm:block">
+                        <div className="flex gap-2">
+                          <div className={workMobile.tech_css}>{workMobile.tech_1}</div>
+                          <div className={workMobile.tech_css}>{workMobile.tech_2}</div>
+                          <div className={workMobile.tech_css}>{workMobile.tech_3}</div>
+                        </div>
+                        <div className="flex gap-2">
+                          <a href={workMobile.link} target="_blank" rel="noreferrer">
+                            <div className="mt-2 rounded-lg bg-zinc-400/10 py-2 px-3 text-sm text-zinc-200 hover:bg-zinc-100 hover:text-zinc-700 dark:text-zinc-600 dark:hover:bg-zinc-300">
+                              GitHub
+                            </div>
+                          </a>
+                          <a href={workMobile.live} target="_blank" rel="noreferrer">
+                            <div className="mt-2 rounded-lg bg-zinc-400/10 py-2 px-3 text-sm text-zinc-200 hover:bg-zinc-100 hover:text-zinc-700 dark:text-zinc-600 dark:hover:bg-zinc-300">
+                              Live
+                            </div>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 );
               })}
