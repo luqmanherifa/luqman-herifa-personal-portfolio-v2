c2ed3531a2476b25c076563455dcbc526cc42f93
luqmanherifa <99471792+luqmanherifa@users.noreply.github.com>
2023-04-19 15:29:25 +0700

    Update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,7 +17,7 @@ function Home() {
         <div>
           <motion.h1
             initial={{ opacity: 0 }}
-            animate={{ y: [-50, 0], opacity: 1 }}
+            animate={{ y: [-100, 0], opacity: 1 }}
             transition={{ delay: 0 }}
             className="mb-6 text-center text-6xl font-extrabold tracking-tight text-white dark:text-slate-900 sm:text-left sm:text-5xl sm:font-bold"
           >
@@ -25,7 +25,7 @@ function Home() {
             <span className="bg-gradient-to-r from-cyan-500 via-indigo-500 to-pink-500 bg-clip-text text-transparent">Luqman Herifa</span>{" "}
             Portfolio.
           </motion.h1>
-          <motion.div initial={{ opacity: 0 }} animate={{ y: [50, 0], opacity: 1 }} transition={{ delay: 0 }}>
+          <motion.div initial={{ opacity: 0 }} animate={{ y: [100, 0], opacity: 1 }} transition={{ delay: 0 }}>
             <p className="text-center text-lg text-slate-400 dark:text-slate-600 sm:text-left">
               I'm Luqman Herifa, a junior software developer specializing in{" "}
               <span className="bg-gradient-to-r from-cyan-400 via-indigo-400 to-pink-400 bg-clip-text text-transparent">front-end web</span>{" "}
