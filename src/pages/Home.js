d914cdb045c60e92d33c53a944db99046ca92f95
luqmanherifa <99471792+luqmanherifa@users.noreply.github.com>
2023-04-19 12:06:53 +0700

    Update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,21 +11,21 @@ function Home() {
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ delay: 0.6 }}
+        transition={{ delay: 0 }}
         className="mx-auto max-w-6xl bg-bgcolors bg-center bg-no-repeat py-28 xl:max-w-4xl lg:max-w-xl lg:bg-none md:max-w-lg md:bg-none sm:max-w-xs sm:bg-none sm:py-10"
       >
         <div>
           <motion.h1
             initial={{ opacity: 0 }}
             animate={{ y: [-50, 0], opacity: 1 }}
-            transition={{ delay: 0.7 }}
+            transition={{ delay: 0 }}
             className="mb-6 text-center text-6xl font-extrabold tracking-tight text-white dark:text-slate-900 sm:text-left sm:text-5xl sm:font-bold"
           >
             Halo, selamat datang di <br className="lg:hidden md:hidden sm:hidden" />{" "}
             <span className="bg-gradient-to-r from-cyan-500 via-indigo-500 to-pink-500 bg-clip-text text-transparent">Luqman Herifa</span>{" "}
             Portfolio.
           </motion.h1>
-          <motion.div initial={{ opacity: 0 }} animate={{ y: [50, 0], opacity: 1 }} transition={{ delay: 0.8 }}>
+          <motion.div initial={{ opacity: 0 }} animate={{ y: [50, 0], opacity: 1 }} transition={{ delay: 0 }}>
             <p className="text-center text-lg text-slate-400 dark:text-slate-600 sm:text-left">
               I'm Luqman Herifa, a junior software developer specializing in{" "}
               <span className="bg-gradient-to-r from-cyan-400 via-indigo-400 to-pink-400 bg-clip-text text-transparent">front-end web</span>{" "}
