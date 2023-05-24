0a100c7ec4b5503001a1a2b013953e9d0db98a72
luqmanherifa <99471792+luqmanherifa@users.noreply.github.com>
2023-05-24 13:53:39 +0700

    Update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,19 +21,29 @@ function Home() {
             transition={{ delay: 0 }}
             className="mb-6 text-center text-6xl font-extrabold tracking-tight text-slate-50 dark:text-slate-900 sm:text-left sm:text-5xl sm:font-bold"
           >
-            Halo, selamat datang di <br className="lg:hidden md:hidden sm:hidden" />{" "}
-            <span className="bg-gradient-to-r from-cyan-500 via-indigo-500 to-pink-500 bg-clip-text text-transparent">Luqman Herifa</span>{" "}
+            Halo, selamat datang di{" "}
+            <br className="lg:hidden md:hidden sm:hidden" />{" "}
+            <span className="bg-gradient-to-r from-cyan-500 via-indigo-500 to-pink-500 bg-clip-text text-transparent">
+              Luqman Herifa
+            </span>{" "}
             Portfolio.
           </motion.h1>
-          <motion.div initial={{ opacity: 0 }} animate={{ y: [100, 0], opacity: 1 }} transition={{ delay: 0 }}>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ y: [100, 0], opacity: 1 }}
+            transition={{ delay: 0 }}
+          >
             <p className="text-center text-lg text-slate-400 dark:text-slate-600 sm:text-left">
               I'm Luqman Herifa, a junior software developer specializing in{" "}
-              <span className="bg-gradient-to-r from-cyan-400 via-indigo-400 to-pink-400 bg-clip-text text-transparent">front-end web</span>{" "}
+              <span className="bg-gradient-to-r from-cyan-400 via-indigo-400 to-pink-400 bg-clip-text text-transparent">
+                front-end web
+              </span>{" "}
               and <br className="lg:hidden md:hidden sm:hidden" />
               <span className="bg-gradient-to-r from-cyan-400 via-indigo-400 to-pink-400 bg-clip-text text-transparent">
                 user interface
               </span>
-              . I just graduated from college and currently looking for new opportunities.
+              . I just graduated from college and currently looking for new
+              opportunities.
             </p>
           </motion.div>
         </div>
