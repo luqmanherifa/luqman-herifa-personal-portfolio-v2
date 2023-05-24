0eeb87410a005a683349e76e1720dd6c51998320
luqmanherifa <99471792+luqmanherifa@users.noreply.github.com>
2023-05-24 14:31:01 +0700

    Update

--- src/components/Elements/Works/Tech.js
+++ src/components/Elements/Works/Tech.js
@@ -2,9 +2,17 @@ const Tech = (props) => {
   const { tech1, tech2, tech3, classname, classname3 = "hidden" } = props;
   return (
     <div className="flex gap-2">
-      <div className={`mt-2 rounded-lg py-2 px-3 text-sm ${classname}`}>{tech1}</div>
-      <div className={`mt-2 rounded-lg py-2 px-3 text-sm ${classname}`}>{tech2}</div>
-      <div className={`mt-2 rounded-lg py-2 px-3 text-sm ${classname3}`}>{tech3}</div>
+      <div className={`mt-2 rounded-lg py-2 px-3 text-sm ${classname}`}>
+        {tech1}
+      </div>
+      <div className={`mt-2 rounded-lg py-2 px-3 text-sm ${classname}`}>
+        {tech2}
+      </div>
+      <div
+        className={`mt-2 rounded-lg py-2 px-3 text-sm line-clamp-1 ${classname3}`}
+      >
+        {tech3}
+      </div>
     </div>
   );
 };
