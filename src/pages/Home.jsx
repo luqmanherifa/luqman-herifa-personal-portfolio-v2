74ff3d6dac6e0b476ef6d2cf60b1e66a4e92df81
Luqman Herifa <luqmanherifa@gmail.com>
2023-07-20 07:55:43 +0700

    Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
             initial={{ opacity: 0 }}
             animate={{ y: [-100, 0], opacity: 1 }}
             transition={{ delay: 0 }}
-            className="mb-6 text-center text-6xl font-extrabold tracking-tight text-slate-50 dark:text-slate-900 sm:text-left sm:text-5xl sm:font-bold"
+            className="mb-6 text-center text-6xl font-extrabold tracking-tight text-white dark:text-slate-900 sm:text-left sm:text-5xl sm:font-bold"
           >
             Halo,
             <br className="hidden sm:block" /> selamat datang di{" "}
