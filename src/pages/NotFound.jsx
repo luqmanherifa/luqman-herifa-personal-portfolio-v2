12b65e802cc61f7a5d65f19ea1415a83b9fff647
Luqman Herifa <luqmanherifa@gmail.com>
2023-11-22 15:48:05 +0700

    Updating works

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,11 +4,11 @@ import { useEffect } from "react";
 
 const NotFound = () => {
   useEffect(() => {
-    document.title = "Ups! Let's Move.";
+    document.title = "Ups! Let's Move";
   }, []);
 
   return (
-    <section className="flex min-h-screen items-center justify-center pb-20">
+    <section className="flex min-h-screen justify-center pt-36 pb-36">
       <div className="text-center xl:max-w-4xl lg:max-w-xl md:max-w-lg sm:max-w-xs">
         <motion.h1
           initial={{ scale: 0 }}
@@ -21,7 +21,8 @@ const NotFound = () => {
           }}
           className="text-6xl font-bold tracking-tight text-slate-100 dark:text-slate-600"
         >
-          Ups! Page not found :(
+          Ups! <br className="hidden lg:block md:block sm:block" />
+          Page not found :(
         </motion.h1>
       </div>
     </section>
