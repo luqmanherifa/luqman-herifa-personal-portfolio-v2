e4f61a88aed08d378765f700c6544cafc6b36ccc
Luqman Herifa <luqmanherifa@gmail.com>
2025-10-22 14:11:04 +0700

    Update

--- src/components/Elements/Navbar/Icon.jsx
+++ src/components/Elements/Navbar/Icon.jsx
@@ -13,7 +13,14 @@ const Icon = () => {
       }}
     >
       <a href="/" className="flex items-center gap-3">
-        <img src="/icon.png" alt="icon" className="w-8 sm:w-7" />
+        <div className="relative">
+          <img
+            src="/icon.png"
+            alt="icon"
+            className="w-7 dark:grayscale sm:w-6"
+          />
+          <div className="absolute inset-0 rounded-full bg-blue-600 opacity-0 dark:opacity-40" />
+        </div>
       </a>
     </motion.div>
   );
