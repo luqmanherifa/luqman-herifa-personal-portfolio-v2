b4c21211335d4c2d8ab4240cc3e93f1f2598ea15
Luqman Herifa <luqmanherifa@gmail.com>
2025-12-30 10:19:54 +0700

    Update

--- src/pages/Explorations.jsx
+++ src/pages/Explorations.jsx
@@ -35,7 +35,7 @@ const Explorations = () => {
         </motion.div>
       </div>
 
-      {/* <div className="hidden bg-red-400/10 text-red-300 dark:text-red-500 bg-orange-400/10 bg-teal-400/10 bg-rose-400/10 bg-sky-400/10 bg-emerald-400/10 bg-slate-400/10 bg-slate-400/10 bg-amber-400/10 bg-green-400/10 text-teal-400 text-rose-400 text-gray-300 text-sky-400 text-emerald-400 text-amber-400 text-orange-400 text-green-400 text-slate-300 line-clamp-3 line-clamp-2 dark:text-gray-500 dark:text-yellow-500 dark:text-blue-100 dark:text-amber-600 bg-sky-400/10 text-sky-300 dark:text-sky-500 bg-red-400/10 text-red-400 bg-orange-400/10 text-orange-400 dark:text-orange-500 bg-lime-400/10 text-lime-400 dark:text-lime-500 bg-indigo-400/10 text-indigo-400"></div> */}
+      {/* <div className="hidden bg-blue-400/10 text-blue-400 bg-red-400/10 text-red-300 dark:text-red-500 bg-orange-400/10 bg-teal-400/10 bg-rose-400/10 bg-sky-400/10 bg-emerald-400/10 bg-slate-400/10 bg-slate-400/10 bg-amber-400/10 bg-green-400/10 text-teal-400 text-rose-400 text-gray-300 text-sky-400 text-emerald-400 text-amber-400 text-orange-400 text-green-400 text-slate-300 line-clamp-3 line-clamp-2 dark:text-gray-500 dark:text-yellow-500 dark:text-blue-100 dark:text-amber-600 bg-sky-400/10 text-sky-300 dark:text-sky-500 bg-red-400/10 text-red-400 bg-orange-400/10 text-orange-400 dark:text-orange-500 bg-lime-400/10 text-lime-400 dark:text-lime-500 bg-indigo-400/10 text-indigo-400"></div> */}
     </section>
   );
 };
